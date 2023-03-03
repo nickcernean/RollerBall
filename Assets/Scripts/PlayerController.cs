21b9080b8653317e805e840ec2d59bc6a2cda344
nicolaecernean <nicolaecer@gmail.com>
2023-03-03 18:13:36 +0100

    created: initial project commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+
+    public TextMeshProUGUI countText;
+
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+
+    private Vector3 movement = new Vector3(0, 0.5f, 0);
+
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        this.count = 0;
+        SetCountText();
+        
+        winTextObject.SetActive(false);
+    }
+
+    public void OnMove(InputAction.CallbackContext movementValue)
+    {
+        Vector2 movementVector = movementValue.ReadValue<Vector2>();
+        
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true); 
+        }
+    }
+     void FixedUpdate()
+    {
+        movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed );
+    }
+
+     private void OnTriggerEnter(Collider other)
+     {
+         if (other.gameObject.CompareTag("PickUp"))
+         {
+             other.gameObject.SetActive(false);
+             count++;
+             SetCountText();
+         }
+        
+     }
+}
